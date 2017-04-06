02347c6dfca706297ac922f38035e93b0ed29c18
Justin Bean <beanjustinr@gmail.com>
2017-04-06 16:01:11 -0400

    show and order user microposts

---
+++ test/models/micropost_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class MicropostTest < ActiveSupport::TestCase
+  def setup
+    @user = users(:michael)
+    @micropost = @user.microposts.create(content: "Lorem ipsum", created_at: 5.hours.ago)
+  end
+
+  test "should be valid" do
+    assert @micropost.valid?
+  end
+
+  test "user id should be present" do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
+
+  test "content should be present" do
+    @micropost.content = "  "
+    assert_not @micropost.valid?
+  end
+
+  test "content should be at most 200 characters" do
+    @micropost.content = "a" * 201
+    assert_not @micropost.valid?
+  end
+
+  test "order should be most recent first" do
+    assert_equal microposts(:most_recent), Micropost.first
+  end
+end
